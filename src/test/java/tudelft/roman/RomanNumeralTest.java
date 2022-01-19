70040f9d4cc7069ecc6b34edfc573b46d30a17c5
maz112233 <maz112233@protonmail.com>
2022-01-19 12:15:28 +0100

    Add @BeforeEach annotation to DRY

--- src/test/java/tudelft/roman/RomanNumeralTest.java
+++ src/test/java/tudelft/roman/RomanNumeralTest.java
@@ -1,15 +1,22 @@
 package tudelft.roman;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class RomanNumeralTest {
 
+    private RomanNumeral roman;
+
+    @BeforeEach
+    public void initialize() {
+        roman = new RomanNumeral();
+    }
 
     @Test // a test must be annotated with this annotation
     public void singleNumber() {
         // Step 1. instantiate the test class (Unit under test).
-        RomanNumeral roman = new RomanNumeral();
+//        RomanNumeral roman = new RomanNumeral();
         // Step 2. invoke the message we want to test.
         int result = roman.convert("I"); // "I" is the test input.
         // Step 3. use assertions to check for equality.
@@ -19,14 +26,12 @@ public class RomanNumeralTest {
     // testing second test scenario, where there are multiple digits in roman numeral.
     @Test
     public void numberWithManyDigits() {
-        RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("VIII");
         Assertions.assertEquals(8, result);
     }
 
     @Test
     public void numberWithSubtractiveNotation() {
-        RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("IV");
         // assertEquals parameters take an order of "expected" followed by "actual".
         Assertions.assertEquals(4, result);
@@ -34,14 +39,12 @@ public class RomanNumeralTest {
 
     @Test
     public void numberWithAndWithoutSubtractiveNotation() {
-        RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("XLIV");
         Assertions.assertEquals(44, result);
     }
 
     @Test
     public void numberWithWrongRepresentation() {
-        RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("VVV");
         Assertions.assertEquals(15, result);
     }
