7653d3f197787a62aeed0af8929fa8545e1ce536
maz112233 <maz112233@protonmail.com>
2022-02-27 11:43:56 +0100

    [Refactor] Use paramterized test to DRY.

--- src/test/java/tudelft/leapyear/LeapYearTest.java
+++ src/test/java/tudelft/leapyear/LeapYearTest.java
@@ -1,73 +1,23 @@
 package tudelft.leapyear;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class LeapYearTest {
 
-	private LeapYear ly;
-	private int candidateYear;
-
-	@BeforeEach
-	public void setup() {
-		ly = new LeapYear();
-	}
-
-	@Test
-	public void leapYearsThatAreNonCenturialYears() {
+	@ParameterizedTest
+	// Test partitions
+	@CsvSource({ "2016,true", "2020,true", "1500,false", "2017,false" })
+	void leapYears(int candidateYear, boolean expected) {
 		// Test preparation
-		candidateYear = 2016;
+		LeapYear uut = new LeapYear();
 
 		// Test actual execution
-		boolean actual = ly.isLeapYear(candidateYear);
+		boolean actual = uut.isLeapYear(candidateYear);
 
 		// Verification
-		Assertions.assertTrue(actual);
-	}
-
-	@Test
-	public void leapCenturialYears() {
-		// Test preparation
-		candidateYear = 2000;
-
-		// Test actual execution
-		boolean actual = ly.isLeapYear(candidateYear);
-
-		// Verification
-		Assertions.assertTrue(actual);
-	}
-
-	private void runTest_positive_cases(int candidateYear) {
-		// Test actual execution
-		boolean actual = ly.isLeapYear(candidateYear);
-
-		// Verification
-		Assertions.assertTrue(actual);
-	}
-
-	@Test
-	public void nonLeapCenturialYears() {
-		// Test preparation
-		candidateYear = 1500;
-
-		runTest_negative_cases(candidateYear);
-	}
-
-	@Test
-	public void nonLeapYears() {
-		// Test preparation
-		candidateYear = 2017;
-
-		runTest_negative_cases(candidateYear);
-	}
-
-	private void runTest_negative_cases(int candidateYear) {
-		// Test actual execution
-		boolean actual = ly.isLeapYear(candidateYear);
-
-		// Verification
-		Assertions.assertFalse(actual);
+		Assertions.assertEquals(expected, actual);
 	}
 
 }
